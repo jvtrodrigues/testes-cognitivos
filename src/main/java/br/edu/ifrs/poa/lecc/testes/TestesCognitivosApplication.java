3c6270e6d4249b9c928a8b0a62a401f5ecd9d7ad
José Rodrigues <josehgunner@gmail.com>
2019-10-02 18:29:09 -0300

    Criacao do projeto

---
+++ src/main/java/br/edu/ifrs/poa/lecc/testes/TestesCognitivosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.poa.lecc.testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesCognitivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesCognitivosApplication.class, args);
+	}
+
+}
